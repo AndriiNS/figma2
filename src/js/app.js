a4bada23729aeb0ac842d919d38dccc61237d3e4
AndriiNS <andriy.naumov05@gmail.com>
2023-09-17 01:26:08 +0300

    the end(+footer+menu+scroll)

--- src/js/app.js
+++ src/js/app.js
@@ -76,11 +76,14 @@ import * as flsForms from './files/forms/forms.js'
 
 /* Работа с полями формы */
 /* Документация: https://template.fls.guru/template-docs/rabota-s-formami.html */
-// flsForms.formFieldsInit({ viewPass: false });
+flsForms.formFieldsInit({
+	viewPass: false,
+	autoHeight: false,
+})
 
 /* Oтправка формы */
 /* Документация: https://template.fls.guru/template-docs/rabota-s-formami.html */
-// flsForms.formSubmit();
+flsForms.formSubmit()
 
 /* Модуль формы "колличество" */
 // flsForms.formQuantity();
@@ -150,14 +153,14 @@ import * as flsForms from './files/forms/forms.js'
 // Наблюдатель за объектами c атрибутом data-watch
 // Документация: https://template.fls.guru/template-docs/modul-nabljudatel-za-poyavleniem-elementa-pri-skrolle.html
 // Сниппет(HTML):
-// import './libs/watcher.js'
+import './libs/watcher.js'
 
 // Функции работы скроллом
 import * as flsScroll from './files/scroll/scroll.js'
 
 // Плавная навигация по странице
 // Документация: https://template.fls.guru/template-docs/modul-plavnoj-navigacii-po-stranice.html
-// flsScroll.pageNavigation();
+flsScroll.pageNavigation()
 
 // Функционал добавления классов к хедеру при прокрутке
 // Документация: https://template.fls.guru/template-docs/modul-dobavleniya-klassov-k-shapke-pri-prokrutke-stranicy.html
